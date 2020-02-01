4922e0eafbcb8509759bb76f5c368a1348caa6b2
Gregory Losco <gtlosco@gmail.com>
2020-02-01 23:52:15 +0000

    Automatically backed up by Learn

--- models/piglatinizer.rb
+++ models/piglatinizer.rb
@@ -1,7 +1,7 @@
 class PigLatinizer 
   
-  def piglatinize(input_string)
-   input_string.strip(" ").length == 1 ? piglatinze_word(input_string) : piglatinize_sentence(input_string)
+  def piglatinize(word)
+   word.strip(" ").length == 1 ? piglatinze_word(word) : piglatinize_sentence(word)
   end
   
   def consonant?(letter)
