81f21cc0a06ebaf9b2b3dee3e92a094ba34ed784
Gregory Losco <gtlosco@gmail.com>
2020-02-02 00:11:57 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -8,7 +8,7 @@ class App < Sinatra::Base
   
   post '/piglatinize' do 
     text = PigLatinizer.new
-    @piglatin = text.piglatinize(params[:input])
+    @piglatin = text.piglatinize(params[:user_phrase])
     
     erb :results
   end 
